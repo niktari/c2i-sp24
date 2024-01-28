38f78509b69e7a796f6680aa770e065b576d40b2
niktari <77365760+niktari@users.noreply.github.com>
2024-01-28 15:55:52 -0500

    update base

--- js/include-html.js
+++ js/include-html.js
@@ -1,19 +1,30 @@
+
+
 // Define the custom <my-include> element
 class MyIncludeElement extends HTMLElement {
 	connectedCallback() {
-		const rel = this.getAttribute("rel");
+	  const rel = this.getAttribute("rel");
+  
+	  // Fetch the content of the specified HTML file
+	  fetch(rel)
+      .then((response) => response.text())
+      .then((data) => {
+        const parsedContent = new DOMParser().parseFromString(data, "text/html").body;
 
-		// Fetch the content of the specified HTML file and replace the current element
-		fetch(rel)
-			.then((response) => response.text())
-			.then((data) =>
-				this.replaceWith(
-					...new DOMParser().parseFromString(data, "text/html").body.childNodes
-				)
-			)
-			.catch((error) => console.error(`Error fetching ${rel}:`, error));
+        // Replace the current element with the parsed content
+        this.replaceWith(...parsedContent.childNodes);
+
+        // Use Promise.resolve() and setTimeout to ensure that replaceWith has completed
+        Promise.resolve().then(() => {
+          setTimeout(() => {
+            interactiveScript();
+          });
+        });
+      })
+      .catch((error) => console.error(`Error fetching ${rel}:`, error));
 	}
-}
-
-// Define the custom element tag with a hyphenated name
-customElements.define("my-include", MyIncludeElement);
+  }
+  
+  // Define the custom element tag with a hyphenated name
+  customElements.define("my-include", MyIncludeElement);
+  
--- js/script.js
+++ js/script.js
@@ -1,153 +1,149 @@
-let palette = ["#FF4901", "#0277C7", "#EDC83E", "#3BB475", "#4E51AA"];
+function interactiveScript() {
+	let palette = ["#FF4901", "#0277C7", "#EDC83E", "#3BB475", "#4E51AA"];
+	
+	// LINKS
+	
+	let links = document.querySelectorAll("a:not(nav ul li a):not(.link--underline)");
+	let underlines = document.querySelectorAll(".link--underline");
+	let summary_links = document.querySelectorAll("summary");
+	
+	console.log(summary_links)
+	
+	links.forEach((link) => {
+	
+		
+	
+		link.onmouseover = () => {
+		let random_color = palette[Math.floor(Math.random() * palette.length)]
+		link.style.color = random_color;
+		
+		}
+	
+		link.onmouseout = () => {
+			link.style.color = "var(--black)";
+			// document.documentElement.style.setProperty("--border", `1.5px solid var(--black)`);
+			}
+	})
+	
+	underlines.forEach((underline => {
+	
+		
+	
+			underline.onmouseover = () => {
+				let random_color = palette[Math.floor(Math.random() * palette.length)]
+				underline.style.color = random_color;
+				underline.style.borderBottom = `1.5px solid ${random_color}`;
+			}
+	
+			underline.onmouseout = () => {
+				underline.style.color = "var(--black)";
+				underline.style.borderBottom = "1.5px solid var(--black)";
+			}
+			
+		}))
+	
+	
+	summary_links.forEach((summary_link) => {
+		summary_link.onmouseover = () => {
+			summary_link.style.color = palette[Math.floor(Math.random() * palette.length)];
+		}
+	
+		summary_link.onmouseout = () => {
+			summary_link.style.color = "var(--black)";
+			}
+	})
+	
+	// HIGHLIGHT
+	window.addEventListener("mousedown", () => {
+		const color = palette.shift();
+		document.documentElement.style.setProperty("--highlight-color", color);
+		palette.push(color);
+		console.log('hi')
+	  });
+	
+	// GLYPHS
+	let c_cells = document.querySelectorAll('#glyph--c .fill');
+	let two_cells = document.querySelectorAll('#glyph--2 .fill');
+	let i_cells = document.querySelectorAll('#glyph--i .fill');
+	
+	
+	c_cells.forEach((c_cell) => {
+		
+		c_cell.onmouseover = () => {
+	
+			c_cell.innerHTML = "<p class='special'>I ♥ Code</p>"
+			c_cell.style.color = palette[Math.floor(Math.random() * palette.length)];
+			c_cell.style.backgroundColor = "transparent";
+			// c_cell.style.transform = `rotate(${e.clientX * 2}deg)`
+			
+			// c_cell.style.backgroundColor = palette[Math.floor(Math.random() * palette.length)];
+			// c_cell.style.borderRadius = "100%";
+	
+			setTimeout(() => {
+				c_cell.innerHTML = "";
+				c_cell.style.backgroundColor = "var(--black)";
+				// c_cell.style.transform = `rotate(0deg)`;
+			}, 1000)
+			
+		}
+	
+	
+	
+	
+	})
+	
+	two_cells.forEach((two_cell) => {
+		two_cell.onmouseover = () => {
+			two_cell.innerHTML = "<p class='special'>I ♥ Code</p>"
+			two_cell.style.color = palette[Math.floor(Math.random() * palette.length)];
+			two_cell.style.backgroundColor = "transparent";
+	
+			setTimeout(() => {
+				two_cell.innerHTML = "";
+				two_cell.style.backgroundColor = "var(--black)";
+			}, 1000)
+		}
+		
+	})
+	
+	i_cells.forEach((i_cell) => {
+	
+		i_cell.onmouseover = () => {
+		i_cell.innerHTML = "<p class='special'>I ♥ Code</p>";
+		i_cell.style.color = palette[Math.floor(Math.random() * palette.length)];
+		i_cell.style.backgroundColor = "transparent";
+		// i_cell.style.top = `${Math.random() * 10 - 5}px`;
+		// i_cell.style.left = `${Math.random() * 10 - 5}px`;
+	
+		setTimeout(() => {
+			i_cell.innerHTML = "";
+			i_cell.style.backgroundColor = "var(--black)";
+		//     i_cell.style.top = 0;
+		// i_cell.style.left = 0;
+		}, 1000)
+		}
+	})
+	
+	let lab = document.getElementById("glyphs--lab");
+	let lab_cells = document.querySelectorAll("#glyphs--lab .fill") 
+	
+		lab.onmouseover = () => {
+			lab_cells.forEach((lab_cell) => {
+				lab_cell.style.backgroundColor = palette[Math.floor(Math.random() * palette.length)];
+				lab_cell.style.top = `${Math.random() * 2}px`;
+				lab_cell.style.left = `${Math.random() * 2}px`;
+			})
+		}
+		
+		lab.onmouseout = () => {
+			lab_cells.forEach((lab_cell) => {
+				lab_cell.style.backgroundColor = "var(--black)";
+				lab_cell.style.top = 0;
+				lab_cell.style.left = 0;
+			})
+		}
+	
 
-// LINKS
-
-let links = document.querySelectorAll("a:not(nav ul li a):not(.link--underline)");
-let underlines = document.querySelectorAll(".link--underline");
-let summary_links = document.querySelectorAll("summary");
-
-console.log(summary_links)
-
-links.forEach((link) => {
-
-    
-
-    link.onmouseover = () => {
-    let random_color = palette[Math.floor(Math.random() * palette.length)]
-    link.style.color = random_color;
-    
-    }
-
-    link.onmouseout = () => {
-        link.style.color = "var(--black)";
-        // document.documentElement.style.setProperty("--border", `1.5px solid var(--black)`);
-        }
-})
-
-underlines.forEach((underline => {
-
-    
-
-        underline.onmouseover = () => {
-            let random_color = palette[Math.floor(Math.random() * palette.length)]
-            underline.style.color = random_color;
-            underline.style.borderBottom = `1.5px solid ${random_color}`;
-        }
-
-        underline.onmouseout = () => {
-            underline.style.color = "var(--black)";
-            underline.style.borderBottom = "1.5px solid var(--black)";
-        }
-        
-    }))
-
-
-summary_links.forEach((summary_link) => {
-    summary_link.onmouseover = () => {
-        summary_link.style.color = palette[Math.floor(Math.random() * palette.length)];
-    }
-
-    summary_link.onmouseout = () => {
-        summary_link.style.color = "var(--black)";
-        }
-})
-
-// HIGHLIGHT
-window.addEventListener("mousedown", () => {
-    const color = palette.shift();
-    document.documentElement.style.setProperty("--highlight-color", color);
-    palette.push(color);
-    console.log('hi')
-  });
-
-// GLYPHS
-let c_cells = document.querySelectorAll('#glyph--c .fill');
-let two_cells = document.querySelectorAll('#glyph--2 .fill');
-let i_cells = document.querySelectorAll('#glyph--i .fill');
-
-
-
-
-
-c_cells.forEach((c_cell) => {
-    
-    c_cell.onmouseover = () => {
-
-        c_cell.innerHTML = "<p class='special'>I ♥ Code</p>"
-        c_cell.style.color = palette[Math.floor(Math.random() * palette.length)];
-        c_cell.style.backgroundColor = "transparent";
-        // c_cell.style.transform = `rotate(${e.clientX * 2}deg)`
-        
-        // c_cell.style.backgroundColor = palette[Math.floor(Math.random() * palette.length)];
-        // c_cell.style.borderRadius = "100%";
-
-        setTimeout(() => {
-            c_cell.innerHTML = "";
-            c_cell.style.backgroundColor = "var(--black)";
-            // c_cell.style.transform = `rotate(0deg)`;
-        }, 1000)
-        
-    }
-
-
-
-
-})
-
-
-
-
-
-two_cells.forEach((two_cell) => {
-    two_cell.onmouseover = () => {
-        two_cell.innerHTML = "<p class='special'>I ♥ Code</p>"
-        two_cell.style.color = palette[Math.floor(Math.random() * palette.length)];
-        two_cell.style.backgroundColor = "transparent";
-
-        setTimeout(() => {
-            two_cell.innerHTML = "";
-            two_cell.style.backgroundColor = "var(--black)";
-        }, 1000)
-    }
-    
-})
-
-i_cells.forEach((i_cell) => {
-
-    i_cell.onmouseover = () => {
-    i_cell.innerHTML = "<p class='special'>I ♥ Code</p>";
-    i_cell.style.color = palette[Math.floor(Math.random() * palette.length)];
-    i_cell.style.backgroundColor = "transparent";
-    // i_cell.style.top = `${Math.random() * 10 - 5}px`;
-    // i_cell.style.left = `${Math.random() * 10 - 5}px`;
-
-    setTimeout(() => {
-        i_cell.innerHTML = "";
-        i_cell.style.backgroundColor = "var(--black)";
-    //     i_cell.style.top = 0;
-    // i_cell.style.left = 0;
-    }, 1000)
-    }
-})
-
-let lab = document.getElementById("glyphs--lab");
-let lab_cells = document.querySelectorAll("#glyphs--lab .fill") 
-
-lab.onmouseover = () => {
-    lab_cells.forEach((lab_cell) => {
-        lab_cell.style.backgroundColor = palette[Math.floor(Math.random() * palette.length)];
-        lab_cell.style.top = `${Math.random() * 2}px`;
-        lab_cell.style.left = `${Math.random() * 2}px`;
-    })
-}
-
-lab.onmouseout = () => {
-    lab_cells.forEach((lab_cell) => {
-        lab_cell.style.backgroundColor = "var(--black)";
-        lab_cell.style.top = 0;
-        lab_cell.style.left = 0;
-    })
-}
-
-
-
+	
+	
+	}
